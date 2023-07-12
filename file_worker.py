dffd25b22d8e0c2fb65d291d11be4d1757c3d6dc
Alex <mwppl123@gmail.com>
2023-07-12 15:40:32 +0200

    checking for new renovations

--- file_worker.py
+++ file_worker.py
@@ -9,9 +9,20 @@ def file_check(new_length):
         if(old_length != new_length):
             for element in active_proceedings:
                 f.write(element)
+            return old_proceedings
 
         #if its the same lenght, its checking for diffrences between saved state and scrapped state
         else:
             if(old_proceedings != active_proceedings):
                 for element in active_proceedings:
                     f.write(element)
+                return old_proceedings
+
+#checking if there were new renovations proceedings
+def contains_objective(old_proceedings):
+    with open("proceedings.txt", "r", encoding="utf-8") as f:
+        new_proceedings = f.readlines()
+        old_renovations = [line for line in old_proceedings if "Remont mieszkań" in line]
+        new_renovations = [line for line in new_proceedings if "Remont mieszkań" in line]
+        diffrence = [line for line in new_renovations if line not in old_renovations]
+        print(diffrence)
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from scrapper import count_of_proceedings
-from file_worker import file_check
+from file_worker import file_check, contains_objective
 
 new_length = count_of_proceedings()
-file_check(new_length)
+old_proceedings = file_check(new_length)
+if old_proceedings != None:
+    contains_objective(old_proceedings)
+else:
+    print("No changes")
