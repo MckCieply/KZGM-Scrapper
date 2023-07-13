89ab87255982d871d95ecb4c8b2e12e09c6f89b0
Alex <mwppl123@gmail.com>
2023-07-13 19:20:45 +0200

    Innit and setting up env variables

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
+from dotenv import load_dotenv
 from scrapper import count_of_proceedings
 from file_worker import file_check, contains_objective
+load_dotenv()
 
 new_length = count_of_proceedings()
 old_proceedings = file_check(new_length)
