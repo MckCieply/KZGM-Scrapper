cbd34787b7dc37982686e5d3d57bc2e6ce7147e0
Alex <mwppl123@gmail.com>
2023-07-13 20:05:19 +0200

    Finished basic version

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import os
-from dotenv import load_dotenv
 from scrapper import count_of_proceedings
 from file_worker import file_check, contains_objective
-load_dotenv()
+from mail_sender import produce_email
 
 new_length = count_of_proceedings()
 old_proceedings = file_check(new_length)
 if old_proceedings != None:
     result = contains_objective(old_proceedings)
     if result == True:
-        pass
-else:
-    print("No changes")
+        produce_email()
+        
