8398bb5e81eb0d0a600090ab99fb8e18cade6050
Alex <mwppl123@gmail.com>
2023-07-14 17:13:55 +0200

    Getting links to send em in email

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from mail_sender import produce_email
 
 new_length = count_of_proceedings()
 old_proceedings = file_check(new_length)
+
+#None is if there is no updates
 if old_proceedings != None:
     result = contains_objective(old_proceedings)
-    if result == True:
-        produce_email()
+
+    #if there is any fitting proceeding proceed to send email
+    if result != []:
+        #Recovering links for all fitting proceedings
+        links = [link.split('|')[1].split('\n')[0] for link in result]
+        produce_email(links)
         
