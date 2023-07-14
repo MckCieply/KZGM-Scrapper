c44c4842012d3f1931180290fc1e94ab2f27c575
Alex <mwppl123@gmail.com>
2023-07-14 17:14:03 +0200

    updating documentation

--- file_worker.py
+++ file_worker.py
@@ -6,6 +6,8 @@ def file_check(new_length):
         active_proceedings = names_of_proceedings()
         #Checking for diffrence in length
         old_length = len(old_proceedings)
+
+        #if there is diffrent lenght in saved and scrapped state rewrite file
         if(old_length != new_length):
             #truncating file
             f.truncate(0)
@@ -17,6 +19,7 @@ def file_check(new_length):
 
         #if its the same lenght, its checking for diffrences between saved state and scrapped state
         else:
+            #if its same length, but with diffrent proceedings, rewrite file
             if(old_proceedings != active_proceedings):
                 #truncating file
                 f.truncate(0)
@@ -33,4 +36,4 @@ def contains_objective(old_proceedings):
         old_renovations = [line for line in old_proceedings if "Remont mieszkań" in line]
         new_renovations = [line for line in new_proceedings if "Remont mieszkań" in line]
         diffrence = [line for line in new_renovations if line not in old_renovations]
-        return diffrence != []
+        return diffrence
